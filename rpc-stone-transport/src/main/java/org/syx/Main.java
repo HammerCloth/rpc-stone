f281f0a07c22f75af3be27c38d202600fafe1e44
siyixiong <18552541076@163.com>
2022-11-21 12:23:48 +0800

    Init-项目初始化

---
+++ rpc-stone-transport/src/main/java/org/syx/Main.java
@@ -0,0 +1,14 @@
+package org.syx;
+
+/**
+ * @ClassName ${NAME}
+ * @Description TODO
+ * @Author SiYiXiong
+ * @Date ${DATE} ${TIME}
+ * @Version v0.1
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
