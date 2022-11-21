00c4eec3c1bc7cb341224e27b27f604d03180b83
siyixiong <18552541076@163.com>
2022-11-21 21:57:18 +0800

    dev-增加readme

--- rpc-stone-transport/src/main/java/org/syx/client/NettyClient.java
+++ rpc-stone-transport/src/main/java/org/syx/client/NettyClient.java
@@ -1,8 +1,11 @@
-package org.syx.client;/**
-* @ClassName NettyClient
-* @Description TODO
-* @Author SiYiXiong
-* @Date 2022/11/21 21:27
-* @Version v0.1
-**/public interface NettyClient {
+package org.syx.client;
+
+/**
+ * @ClassName NettyClient
+ * @Description 在传输层，一个客户端最简单的指责就是发送请求，获得响应
+ * @Author SiYiXiong
+ * @Date 2022/11/21 21:27
+ * @Version v0.1
+ **/
+public interface NettyClient {
 }
--- rpc-stone-transport/src/main/java/org/syx/server/NettyServer.java
+++ rpc-stone-transport/src/main/java/org/syx/server/NettyServer.java
@@ -1,8 +1,11 @@
-package org.syx.server;/**
-* @ClassName NettyServer
-* @Description TODO
-* @Author SiYiXiong
-* @Date 2022/11/21 21:28
-* @Version v0.1
-**/public interface NettyServer {
+package org.syx.server;
+
+/**
+ * @ClassName NettyServer
+ * @Description 一个简单的服务器，就是接收请求，发送响应
+ * @Author SiYiXiong
+ * @Date 2022/11/21 21:28
+ * @Version v0.1
+ **/
+public interface NettyServer {
 }
