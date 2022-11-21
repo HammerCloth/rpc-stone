caf68de3106027696bdfde9bb7930cb807ca8c27
siyixiong <18552541076@163.com>
2022-11-21 21:54:10 +0800

    feat-增加接口文件，以及相关记录

---
+++ rpc-stone-transport/src/main/java/org/syx/client/NettyClient.java
@@ -0,0 +1,8 @@
+package org.syx.client;/**
+* @ClassName NettyClient
+* @Description TODO
+* @Author SiYiXiong
+* @Date 2022/11/21 21:27
+* @Version v0.1
+**/public interface NettyClient {
+}
---
+++ rpc-stone-transport/src/main/java/org/syx/server/NettyServer.java
@@ -0,0 +1,8 @@
+package org.syx.server;/**
+* @ClassName NettyServer
+* @Description TODO
+* @Author SiYiXiong
+* @Date 2022/11/21 21:28
+* @Version v0.1
+**/public interface NettyServer {
+}
